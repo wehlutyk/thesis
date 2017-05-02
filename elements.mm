7f0420194938dc4d1e1d4364516f0c82c10ba39d
Sébastien Lerique <sl@mehho.net>
2017-05-02 16:59:41 +0200

    feat/maps: add elements for the outline, and a better outline (plan2)

---
+++ elements.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1493575664217" ID="ID_1243337903" MODIFIED="1493576979945" TEXT="Elements">
+<node CREATED="1493575713352" ID="ID_164981892" MODIFIED="1493575741041" POSITION="right" TEXT="Looking to connect anthropology and cognitive science"/>
+<node CREATED="1493575743773" ID="ID_1739875516" MODIFIED="1493645553242" POSITION="right" TEXT="Sperber proposes that (explain focus on him). What is there to bring? Testing."/>
+<node CREATED="1493577488964" ID="ID_1705225146" MODIFIED="1493645529162" POSITION="right" TEXT="Literature on Sperber, competing approaches, empirical work"/>
+<node CREATED="1493576328100" ID="ID_1124338306" MODIFIED="1493576329923" POSITION="right" TEXT="MT"/>
+<node CREATED="1493576331384" ID="ID_510931363" MODIFIED="1493576336484" POSITION="right" TEXT="Gistr"/>
+<node CREATED="1493575674274" ID="ID_193169863" MODIFIED="1493575708086" POSITION="right" TEXT="Under-specified CV problem">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1493576093254" ID="ID_1033465813" MODIFIED="1493576096670" TEXT="What level"/>
+<node CREATED="1493576099834" ID="ID_874337451" MODIFIED="1493576109972" TEXT="What criterion for yes/no"/>
+<node CREATED="1493578059852" ID="ID_660237896" MODIFIED="1493578100240" TEXT="Maybe a consequence of not wanting a GUT, but that doesn&apos;t bring theory then"/>
+</node>
+<node CREATED="1493575887793" ID="ID_111772960" MODIFIED="1493576219397" POSITION="right" TEXT="Context is not very present in Sperber. Is in RT."/>
+<node CREATED="1493575936461" ID="ID_1756103174" MODIFIED="1493576297981" POSITION="right" TEXT="Problem of interpretation/meaning/content/representation: what level do you choose to look at? What is a cultural trait? We must choose which meaning."/>
+<node CREATED="1493575810140" ID="ID_1842097919" MODIFIED="1493576376384" POSITION="right" TEXT="Ingold criticises dualism. Connects with DST and 4E."/>
+<node CREATED="1493575901126" ID="ID_440093817" MODIFIED="1493576246326" POSITION="right" STYLE="fork" TEXT="Sterelny&apos;s cumulative niche-construction is a nice addition to Sperber"/>
+<node CREATED="1493645447557" ID="ID_742368448" MODIFIED="1493645480461" POSITION="right" TEXT="We should unite forces by making reusable components, as big experiments are coming"/>
+<node CREATED="1493577752672" ID="ID_1877832072" MODIFIED="1493577871394" POSITION="left" TEXT="I think there is room for intelligible descriptions of how human groups are, how culture changes, is, develops"/>
+<node CREATED="1493577791915" ID="ID_1069878653" MODIFIED="1493577814969" POSITION="left" TEXT="Sperber brings much in that direction: intelligibility with good concepts"/>
+<node CREATED="1493577929160" ID="ID_513114199" MODIFIED="1493577937080" POSITION="left" TEXT="There are empirical questions"/>
+<node CREATED="1493577965595" ID="ID_1914172978" MODIFIED="1493577971467" POSITION="left" TEXT="I have tried a couple"/>
+<node CREATED="1493577615097" ID="ID_284408713" MODIFIED="1493577989571" POSITION="left" TEXT="And reached the conclusion that to move forward, we need to attack meaning/content, with all its levels, and remove dualisms. I hope to convince you."/>
+</node>
+</map>
---
+++ plan2.mm
@@ -0,0 +1,218 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1493651772244" ID="ID_1234760764" MODIFIED="1493735987484" TEXT="Plan">
+<node COLOR="#ff0000" CREATED="1493671863081" ID="ID_730295271" MODIFIED="1493719478603" POSITION="right" TEXT="Overarching goal">
+<node CREATED="1493733689038" ID="ID_354172035" MODIFIED="1493733915158" TEXT="Contribute tests to CAT as a viable cog-soc synthesis"/>
+<node CREATED="1493733867440" ID="ID_1667893945" MODIFIED="1493735670937" TEXT="Discuss shortcomings of my approach, shortcomings of CAT beyond my approach, and the role of experiments to move forward"/>
+</node>
+<node CREATED="1493651794390" ID="ID_636762157" MODIFIED="1493651798803" POSITION="right" TEXT="Introduction">
+<node CREATED="1493651847942" ID="ID_476400367" MODIFIED="1493736031900" TEXT="The theme">
+<node CREATED="1493651858455" ID="ID_1905816666" MODIFIED="1493733796696" TEXT="How I got there: wanting to connect cognitive science and social science"/>
+<node CREATED="1493651874130" ID="ID_100362675" MODIFIED="1493659452557" TEXT="Ultimate goal: not subsuming, first walking the gap, then maybe a synthesis"/>
+</node>
+<node CREATED="1468500992272" ID="ID_1210267072" MODIFIED="1493736031905" TEXT="Literature">
+<node CREATED="1468501015091" ID="ID_1175182254" MODIFIED="1493736031910" TEXT="Cog + Soc in the 20th">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1468501073029" ID="ID_270646699" MODIFIED="1468501141979" TEXT="Psychology: Lewis, Kroeber, Gibson"/>
+<node COLOR="#999999" CREATED="1493652068205" ID="ID_1768570116" MODIFIED="1493652589428" TEXT="Behaviourism / birth of Cognitive Science">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node CREATED="1468501078311" ID="ID_1545179629" MODIFIED="1468508471692" TEXT="Sociology and Anthropology: Durkheim, Mauss, Simmel, Garfinkel, Geertz, Bourdieu, Giddens"/>
+</node>
+<node CREATED="1468501391707" ID="ID_677606115" MODIFIED="1493736031912" TEXT="Dual Inheritance Theory">
+<node CREATED="1493652130573" ID="ID_398585210" MODIFIED="1493736031913" TEXT="Classical Cultural Evolution">
+<node CREATED="1468501404617" ID="ID_802296269" MODIFIED="1468501433336" TEXT="Cavalli-Sforza &amp; Feldman"/>
+<node CREATED="1468501437913" ID="ID_1979800518" MODIFIED="1468501553126" TEXT="Boyd, Richerson &amp; Henrich"/>
+<node CREATED="1468501442078" ID="ID_1162369254" MODIFIED="1468501452789" TEXT="Dawkins"/>
+<node CREATED="1468501528422" ID="ID_390258329" MODIFIED="1468502000857" TEXT="Mesoudi &amp; Whiten "/>
+</node>
+<node CREATED="1493652111065" ID="ID_302804498" MODIFIED="1493736031915" TEXT="Cultural Attraction Theory">
+<node CREATED="1493652486183" ID="ID_1521128903" MODIFIED="1493652491538" TEXT="Scott-Phillips"/>
+<node CREATED="1468501453245" ID="ID_1216015690" MODIFIED="1468502446629" TEXT="Sperber, Claidi&#xe8;re, Mercier, O. Morin"/>
+<node CREATED="1468501526765" ID="ID_1404723662" MODIFIED="1468502449892" TEXT="Baumard, Boyer"/>
+</node>
+</node>
+<node CREATED="1493656507622" ID="ID_160050517" MODIFIED="1493736031919" TEXT="Neighbouring areas">
+<node CREATED="1468501473713" ID="ID_742989681" MODIFIED="1493736031920" TEXT="Language Evolution">
+<node CREATED="1468501618173" ID="ID_1220410164" MODIFIED="1493653824168" TEXT="Kirby, Cornish, Kalish, Tamariz, Steels: how structure emerges from random"/>
+<node CREATED="1468501772184" ID="ID_154045820" MODIFIED="1493653842213" TEXT="Croft: how existing structure evolves"/>
+</node>
+<node CREATED="1468501819507" ID="ID_1905159781" MODIFIED="1493736031921" TEXT="Online data">
+<node CREATED="1468501855163" ID="ID_1053079280" MODIFIED="1468501856495" TEXT="Danescu-Niculescu-Mizil"/>
+<node CREATED="1468501857254" ID="ID_1942990214" MODIFIED="1468502487897" TEXT="Kleinberg, Leskovec, Adamic, Simmons"/>
+<node CREATED="1468502488528" ID="ID_1356760065" MODIFIED="1468502517402" TEXT="Cointet, Roth, Omodei, Cardon"/>
+</node>
+<node CREATED="1493652800312" ID="ID_1801798611" MODIFIED="1493652869776" TEXT="Information diffusion on networks">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1468508459957" ID="ID_1750851331" MODIFIED="1493736031922" TEXT="Complex Systems">
+<node CREATED="1468508476236" ID="ID_1217369841" MODIFIED="1468508477258" TEXT="E. Morin"/>
+<node CREATED="1468508478948" ID="ID_1000231616" MODIFIED="1468508482940" TEXT="Santa Fe people"/>
+</node>
+<node CREATED="1493653687097" ID="ID_211664543" MODIFIED="1493714498697" TEXT="Literature we&apos;ll see later">
+<node CREATED="1493653308795" ID="ID_459860988" MODIFIED="1493653704861" TEXT="Psycholinguistics">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1468510510941" ID="ID_874854629" MODIFIED="1468510514855" TEXT="Smartphone psychology"/>
+</node>
+</node>
+<node CREATED="1493656469691" ID="ID_1239768678" MODIFIED="1493736031924" TEXT="Criticisms, parallels, alternatives">
+<node CREATED="1493652652059" ID="ID_1233298331" MODIFIED="1493736031925" TEXT="Social Anthropology">
+<node COLOR="#006699" CREATED="1493652676709" ID="ID_1667765208" MODIFIED="1493652694480" TEXT="Stotz"/>
+<node CREATED="1493652662611" ID="ID_43955621" MODIFIED="1493652664489" TEXT="Ingold"/>
+<node CREATED="1493652664961" ID="ID_1001260418" MODIFIED="1493652666677" TEXT="Sinha"/>
+<node CREATED="1493652667142" ID="ID_1805536813" MODIFIED="1493652669293" TEXT="Donald"/>
+<node CREATED="1468501919206" ID="ID_1149253302" MODIFIED="1468501920529" TEXT="Briggs"/>
+</node>
+<node CREATED="1468501749770" ID="ID_916482541" MODIFIED="1493736031927" TEXT="4E / Enactive">
+<node CREATED="1468501783892" ID="ID_98150479" MODIFIED="1468502049140" TEXT="Di Paolo, De Jaegher &amp; Cuffari"/>
+<node CREATED="1493652734433" ID="ID_1217468863" MODIFIED="1493652740638" TEXT="Thompson, Varela"/>
+<node CREATED="1468501807980" ID="ID_661726123" MODIFIED="1468501810831" TEXT="Wittgenstein"/>
+<node CREATED="1493660001231" ID="ID_485121586" MODIFIED="1493660006620" TEXT="Sterelny, Stotz"/>
+</node>
+<node CREATED="1468501815379" ID="ID_177877370" MODIFIED="1493656272401" TEXT="Evo-devo &amp; Eco-evo-devo &amp; Developmental Systems Theory"/>
+</node>
+</node>
+<node CREATED="1493651988017" ID="ID_670111761" MODIFIED="1493736031928" TEXT="Open problems">
+<node CREATED="1493653437891" ID="ID_67444389" MODIFIED="1493653475887" TEXT="Attraction vs. Selection"/>
+<node CREATED="1493653037758" ID="ID_11239510" MODIFIED="1493656591532" TEXT="Actual attractors"/>
+<node CREATED="1493653044924" ID="ID_1768318567" MODIFIED="1493653620224" TEXT="Context &amp; Niche construction"/>
+<node CREATED="1493655549620" ID="ID_184421290" MODIFIED="1493655553163" TEXT="What are traits"/>
+<node CREATED="1493653058625" ID="ID_117276132" MODIFIED="1493653062331" TEXT="More case studies"/>
+</node>
+<node COLOR="#ff0000" CREATED="1493652975329" ID="ID_167583603" MODIFIED="1493721161078" TEXT="Stakes">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1493653721774" ID="ID_283410491" MODIFIED="1493654448011" TEXT="At the outset: empirically test for attractors by focusing on short utterances"/>
+<node CREATED="1493653730048" ID="ID_1638247989" MODIFIED="1493654497692" TEXT="At the end">
+<node CREATED="1493654484979" ID="ID_1947565210" MODIFIED="1493670636264" TEXT="Practical demonstration of empirical studies: results, costs incurred, and tools produced"/>
+<node CREATED="1493654502046" ID="ID_509470590" MODIFIED="1493714604441" TEXT="Highlight shortcomings and avenues to move forward"/>
+</node>
+</node>
+</node>
+<node CREATED="1493653568209" ID="ID_1225066825" MODIFIED="1493719455487" POSITION="right" TEXT="MemeTracker">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#338800" CREATED="1493719401794" ID="ID_337599956" MODIFIED="1493719445278" TEXT="Added value: connect cultural evolution and psycholinguistics using online data"/>
+<node CREATED="1493654029986" ID="ID_1862644501" MODIFIED="1493654033635" TEXT="Introduction"/>
+<node CREATED="1493654035198" ID="ID_455617458" MODIFIED="1493654037781" TEXT="Related Work"/>
+<node CREATED="1493654046205" ID="ID_632969968" MODIFIED="1493736031930" TEXT="Methods">
+<node CREATED="1493654050133" ID="ID_808723623" MODIFIED="1493654058226" TEXT="Corpus-based utterances"/>
+<node CREATED="1493654060026" ID="ID_543057533" MODIFIED="1493654064039" TEXT="Word-level measures"/>
+<node CREATED="1493654065203" ID="ID_1930493293" MODIFIED="1493654069439" TEXT="Substitution model"/>
+<node CREATED="1493716486166" ID="ID_1826012162" MODIFIED="1493716490360" TEXT="Spam-filtering"/>
+<node CREATED="1493715136014" ID="ID_1350313092" MODIFIED="1493719279506" TEXT="Bugs &amp; Best practices"/>
+<node CREATED="1493715175752" ID="ID_1056681756" MODIFIED="1493715187618" TEXT="Aborted transmission chains"/>
+</node>
+<node CREATED="1493654071684" ID="ID_1692376312" MODIFIED="1493736031932" TEXT="Results">
+<node CREATED="1493654076734" ID="ID_1555998712" MODIFIED="1493716665212" TEXT="Susceptibility for POS / features"/>
+<node CREATED="1493654083853" ID="ID_232568325" MODIFIED="1493716656558" TEXT="Variation for features"/>
+<node CREATED="1493654087506" ID="ID_1370070616" MODIFIED="1493718021500" TEXT="Sentence context">
+<node CREATED="1493718024505" ID="ID_482813499" MODIFIED="1493718038318" TEXT="Quantile susceptibility"/>
+<node CREATED="1493718039283" ID="ID_463958121" MODIFIED="1493718042353" TEXT="Sentence-relative variation"/>
+</node>
+</node>
+<node CREATED="1493654094088" ID="ID_1489096018" MODIFIED="1493654097525" TEXT="Discussion"/>
+<node CREATED="1493654098650" ID="ID_1878195409" MODIFIED="1493654108321" TEXT="Concluding remarks"/>
+</node>
+<node CREATED="1493653580005" ID="ID_281753914" MODIFIED="1493735443355" POSITION="right" TEXT="Gistr">
+<node COLOR="#ff0000" CREATED="1493671851190" ID="ID_367396253" MODIFIED="1493719468812" TEXT="Added value">
+<node CREATED="1493671928289" ID="ID_455315078" MODIFIED="1493671945176" TEXT="Decide once the results are stable"/>
+</node>
+<node CREATED="1493669206627" ID="ID_1036336811" MODIFIED="1493736031934" TEXT="Introduction">
+<node CREATED="1493671327434" ID="ID_326009337" MODIFIED="1493671487515" TEXT="Going for the evolution of content, not only a contrast"/>
+<node CREATED="1493671341904" ID="ID_318906586" MODIFIED="1493671788385" TEXT="With several levels of questions">
+<node CREATED="1493671407213" ID="ID_280809433" MODIFIED="1493671623825" TEXT="Simple effects of length and transmissibility"/>
+<node CREATED="1493671356458" ID="ID_883434153" MODIFIED="1493671653749" TEXT="Reproduction and extension of word-level effects"/>
+<node CREATED="1493671627788" ID="ID_1041424065" MODIFIED="1493671677164" TEXT="Stability &amp; disappearances"/>
+<node CREATED="1493671549406" ID="ID_476325565" MODIFIED="1493671567033" TEXT="Case-specific studies"/>
+<node CREATED="1493671793153" ID="ID_803300195" MODIFIED="1493671796164" TEXT="Variability"/>
+</node>
+</node>
+<node CREATED="1493669606965" ID="ID_1361068873" MODIFIED="1493669609294" TEXT="Related Work"/>
+<node CREATED="1493669627078" ID="ID_1867332189" MODIFIED="1493736031935" TEXT="General Methods">
+<node CREATED="1493669232382" ID="ID_1471337441" MODIFIED="1493669644648" TEXT="Experiment design principles"/>
+<node CREATED="1493669236975" ID="ID_1560804079" MODIFIED="1493669522736" TEXT="Trials and errors in the design"/>
+</node>
+<node COLOR="#006699" CREATED="1493670260486" ID="ID_1835673601" MODIFIED="1493735454925" TEXT="Elements to fit in">
+<node CREATED="1493670174797" ID="ID_537282413" MODIFIED="1493736031936" TEXT="Choice of sources">
+<node CREATED="1493670342185" ID="ID_416720077" MODIFIED="1493670377413" TEXT="1: From MT, famous quotes, hand-designed sentences"/>
+<node CREATED="1493670380463" ID="ID_1714474394" MODIFIED="1493670392670" TEXT="2: From memorable/non-memorable quotes"/>
+<node CREATED="1493670395149" ID="ID_323088763" MODIFIED="1493670404587" TEXT="3: From short stories"/>
+</node>
+<node CREATED="1493670170670" ID="ID_1200065850" MODIFIED="1493735463488" TEXT="High-level questions">
+<node CREATED="1493670312549" ID="ID_113996890" MODIFIED="1493735473925" TEXT="Exp 1: Initially, testing for convergence"/>
+<node CREATED="1493670327524" ID="ID_567804241" MODIFIED="1493735488470" TEXT="Exp 2: Exploring effect and evolution of memorability"/>
+<node CREATED="1493670457098" ID="ID_1147811019" MODIFIED="1493735522093" TEXT="Exp 3: Testing for standardised simplification of content, and some case-specific effects"/>
+</node>
+<node CREATED="1493670301784" ID="ID_1992263097" MODIFIED="1493670304864" TEXT="Results">
+<node COLOR="#ff0000" CREATED="1493708779272" ID="ID_1876306470" MODIFIED="1493735536077" TEXT="We find ..."/>
+</node>
+</node>
+<node CREATED="1493670279014" ID="ID_744932518" MODIFIED="1493670281597" TEXT="Discussion"/>
+</node>
+<node CREATED="1493654137848" ID="ID_1808416012" MODIFIED="1493721108221" POSITION="right" TEXT="Contribution Discussion">
+<node COLOR="#ff0000" CREATED="1493671839179" ID="ID_1238430561" MODIFIED="1493721066670" TEXT="Added value">
+<node CREATED="1493709245536" ID="ID_1764838917" MODIFIED="1493709287044" TEXT="1) Experiments can still bring a whole lot (everybody agrees on that)"/>
+<node CREATED="1493709548787" ID="ID_1914652251" MODIFIED="1493709713921" TEXT="2) But they skirt around the problem of content"/>
+<node CREATED="1493709889382" ID="ID_755951728" MODIFIED="1493711701325" TEXT="3) Some existing criticisms offer an explanation to that deadlock">
+<node CREATED="1493710145866" ID="ID_194905913" MODIFIED="1493711717008" TEXT="They throw out the baby with the bathwater by not wanting to bridge terminology"/>
+<node CREATED="1493710151373" ID="ID_992572604" MODIFIED="1493711725263" TEXT="But they can be boiled down to a set of workable levels, to foster dialogue"/>
+</node>
+<node CREATED="1493709856098" ID="ID_1992374704" MODIFIED="1493711109051" TEXT="4) It can be worthwhile to set up experiments based on existing criticisms">
+<node COLOR="#ff0000" CREATED="1493710780051" ID="ID_1824948192" MODIFIED="1493711426152" TEXT="What exactly?"/>
+</node>
+</node>
+<node CREATED="1493655409373" ID="ID_1331299227" MODIFIED="1493736031939" TEXT="Synthesis of positive results">
+<node CREATED="1493669429075" ID="ID_295290723" MODIFIED="1493669430540" TEXT="MT">
+<node CREATED="1493669331808" ID="ID_1224440570" MODIFIED="1493669476674" TEXT="Patterns compatible with convergence, applying CAT to online behaviour"/>
+<node CREATED="1493669398214" ID="ID_1241198916" MODIFIED="1493669441808" TEXT="Proof-of-concept for connecting online data to psycholinguistics"/>
+</node>
+<node CREATED="1493669481954" ID="ID_1839179845" MODIFIED="1493669483283" TEXT="Gistr"/>
+</node>
+<node CREATED="1493708807781" ID="ID_886737445" MODIFIED="1493736031946" TEXT="Many unexplored possibilities">
+<node CREATED="1493708828871" ID="ID_1834477044" MODIFIED="1493719683071" TEXT="Dependency on the ordering of subjects (note some of Kirby&apos;s results depend on that)"/>
+<node CREATED="1493708840626" ID="ID_90314491" MODIFIED="1493720965819" TEXT="Task dependency: changing the reward function, changing the framing in instructions"/>
+<node CREATED="1493708877446" ID="ID_998035384" MODIFIED="1493708977867" TEXT="Adding a distraction task between reading and writing"/>
+<node CREATED="1493708886425" ID="ID_183030865" MODIFIED="1493720938608" TEXT="Adding surrounding context to utterances (Zaromb, Potter &amp; Lombardi, etc.)"/>
+<node CREATED="1493708898450" ID="ID_1035097220" MODIFIED="1493719732446" TEXT="Adding 1-1 interactions where subjects have an incentive to write something sensible (ref. Claidi&#xe8;re argumentation)"/>
+<node CREATED="1493709006933" ID="ID_355813653" MODIFIED="1493709026086" TEXT="Using utterances that work as a system to influence each other (&#xe0; la Kirby)"/>
+<node CREATED="1493709293236" ID="ID_533098974" MODIFIED="1493736031947" TEXT="Some kind of niche contstruction / environment engineering?">
+<node CREATED="1493709487313" ID="ID_842164103" MODIFIED="1493714830179" TEXT="By letting subjects observe the sentences of others, or observe other subjects doing the task?">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1493655628727" ID="ID_1558587722" MODIFIED="1493711157277" TEXT="Why didn&apos;t I do it? Cost of operationalising">
+<node CREATED="1493669492879" ID="ID_718896090" MODIFIED="1493670769578" TEXT="MT inference"/>
+<node CREATED="1493669498178" ID="ID_1857083375" MODIFIED="1493670764232" TEXT="Gistr engineering"/>
+</node>
+</node>
+<node CREATED="1493709128157" ID="ID_985105476" MODIFIED="1493717850292" TEXT="Review the &quot;No Theory of Content&quot; problem">
+<node CREATED="1493717779005" ID="ID_1984119433" MODIFIED="1493736031951" TEXT="In my work">
+<node CREATED="1493657060913" ID="ID_398917637" MODIFIED="1493717799258" TEXT="Surfaces as the meaning/interpretation problem"/>
+<node CREATED="1493575674274" ID="ID_193169863" MODIFIED="1493717868154" TEXT="And leads to an under-specified CV problem">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1493576093254" ID="ID_1033465813" MODIFIED="1493656003801" TEXT="What level do you choose to look at"/>
+<node CREATED="1493578059852" ID="ID_660237896" MODIFIED="1493578100240" TEXT="Maybe a consequence of not wanting a GUT, but that doesn&apos;t bring theory then"/>
+</node>
+<node CREATED="1493719848890" ID="ID_100848672" MODIFIED="1493720202114" TEXT="Divides into: content problem, context problem, dynamics problem (critics argue these are all the same)"/>
+</node>
+<node CREATED="1493709180550" ID="ID_1655961620" MODIFIED="1493719880857" TEXT="For context, there is Relevance Theory">
+<node CREATED="1493712374053" ID="ID_984289985" MODIFIED="1493712397963" TEXT="Not aware of implementations, but could work in principle"/>
+<node CREATED="1493712013029" ID="ID_1960379021" MODIFIED="1493712122191" TEXT="I want to leave aside, because it&apos;s based on formal semantics, and I don&apos;t see how it will be able to explain fuzzy things like poetry"/>
+<node COLOR="#ff0000" CREATED="1493709727697" ID="ID_395537700" MODIFIED="1493711861374" TEXT="Not entirely satisfying. Should I discuss in depth?"/>
+</node>
+<node CREATED="1493575936461" ID="ID_1756103174" MODIFIED="1493736031953" TEXT="Content problem is a philosophical debate (Millikan etc.), but also seen as an &quot;Information Dualism&quot; by critics of CAT itself">
+<node CREATED="1493656191241" ID="ID_1194521248" MODIFIED="1493720901946" TEXT="Social Anthropology (Ingold, Sinha, Donald, Toren)"/>
+<node CREATED="1493656199084" ID="ID_478691745" MODIFIED="1493720010353" TEXT="Indirectly (through critique of classical cognitive science): 4E / Enactive"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1493659820803" ID="ID_1253220005" MODIFIED="1493721196536" TEXT="Moving forward">
+<node CREATED="1493712218022" ID="ID_866917372" MODIFIED="1493712225971" TEXT="Niche contstruction is a first bridge">
+<node CREATED="1493715024581" ID="ID_501934080" MODIFIED="1493715058969" TEXT="It sets some focus on how the learning environment is something that evolves and is transmitted"/>
+<node CREATED="1493720998924" ID="ID_640101377" MODIFIED="1493721011527" TEXT="It opens to a larger cycle"/>
+</node>
+<node CREATED="1493659841429" ID="ID_1074701157" MODIFIED="1493659865992" TEXT="Teasing apart approaches to meaning"/>
+<node CREATED="1493659868199" ID="ID_634762012" MODIFIED="1493659886390" TEXT="Sharing tools to empower"/>
+</node>
+</node>
+</node>
+</map>
